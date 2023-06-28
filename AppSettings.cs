47cb935133014cb9e7b7fefb40fc52ad612c75e0
Kapu <kapu1500@gmail.com>
2023-06-28 20:15:34 +0200

    Initial commit

---
+++ AppSettings.cs
@@ -0,0 +1,17 @@
+﻿using DotaHead.Services;
+
+namespace DotaHead;
+
+public class AppSettings
+{
+    public ulong GuildId { get; set; }
+    public ulong ChannelId { get; set; }
+    public string DiscordToken { get; set; }
+    public int PeakHoursStart { get; set; }
+    public int PeakHoursEnd { get; set; }
+    public int PeakHoursRefreshTime { get; set; }
+    public int NormalRefreshTime { get; set; }
+
+
+    public List<UserRecord> Players { get; set; }
+}
---
+++ Services/PlayersService.cs
@@ -0,0 +1,18 @@
+﻿namespace DotaHead.Services;
+
+public class PlayersService
+{
+    public List<UserRecord> PlayerIds { get; set; }
+
+    public PlayersService(AppSettings appSettings)
+    {
+        PlayerIds = appSettings.Players;
+    }
+}
+
+public class UserRecord
+{
+    public long DiscordId { get; set; }
+    public long DotaId { get; set; }
+
+}
